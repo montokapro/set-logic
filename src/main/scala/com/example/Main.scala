9fb938ae43078e63b21e29f37baebdc28af264a5
Stephen Webber <montokapro@gmail.com>
2020-03-29 20:06:26 -0500

    Add trampolining for stack safety

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -3,7 +3,8 @@ package com.example
 import cats._
 
 object SetLogic {
-  import cats.free.{ Free => Free_}
+  import cats.free.Trampoline
+  import cats.free.Trampoline._
 
   sealed trait Term
   case class Literal[T](value: T) extends Term
@@ -12,11 +13,11 @@ object SetLogic {
   case class Or(value: List[Term]) extends Term
 
   // pure
-  def compile(term: Term): Term = term match {
-    case Not(Not(value)) => compile(value)
-    case And(list: List[Term]) if list.size == 1 => compile(list.head)
-    case Or(list: List[Term]) if list.size == 1 => compile(list.head)
-    case value => value
+  def compile(term: Term): Trampoline[Term] = term match {
+    case Not(Not(value)) => defer(compile(value))
+    case And(list: List[Term]) if list.size == 1 => defer(compile(list.head))
+    case Or(list: List[Term]) if list.size == 1 => defer(compile(list.head))
+    case value => done(value)
   }
 }
 
@@ -28,20 +29,22 @@ object SetLogic {
 // scala> import com.example.Main
 // import com.example.Main
 //
-// scala> Main.run
+// scala> Main.runLiteral
 //
 object Main {
+  import cats.implicits._
+
   // Note that the pure approach is a unique in that the state is simply an Int
   // as opposed to a Counter containing an Int
-  def runLiteral = SetLogic.compile(SetLogic.Literal(1))
+  def runLiteral = SetLogic.compile(SetLogic.Literal(1)).run
 
-  def runNot = SetLogic.compile(SetLogic.Not(SetLogic.Not(SetLogic.Literal(1))))
+  def runNot = SetLogic.compile(SetLogic.Not(SetLogic.Not(SetLogic.Literal(1)))).run
 
   def runRecursive = SetLogic.compile(
     SetLogic.Not(SetLogic.Not(SetLogic.Not(SetLogic.Not(SetLogic.Not(SetLogic.Literal(1))))))
-  )
+  ).run
 
-  def runAndSingle = SetLogic.compile(SetLogic.And(List(SetLogic.Literal(1))))
+  def runAndSingle = SetLogic.compile(SetLogic.And(List(SetLogic.Literal(1)))).run
 
-  def runOrSingle = SetLogic.compile(SetLogic.Or(List(SetLogic.Literal(1))))
+  def runOrSingle = SetLogic.compile(SetLogic.Or(List(SetLogic.Literal(1)))).run
 }
